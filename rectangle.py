5fb1224a51ff20dea75e2adfefc6da7b75c7c8b6
mmartorelluria <mmartorelluria@seu.edu>
2024-03-25 16:09:11 -0400

    implementing rectangle.py

--- rectangle.py
+++ rectangle.py
@@ -3,21 +3,25 @@ from shape import Shape
 class Rectangle(Shape):
 
     def __init__(self, a, b):
-        ...
+        self.check_if_args_not_below_zero(a,b)
+        self.a = a
+        self.b = b
 
     def get_area(self):
-        return 0
+        area = self.a * self.b
+        return area
 
     def get_perimeter(self):
-        return 0
+        perimeter = 2 * (self.a + self.b)
+        return perimeter
 
     def __str__(self):
-        return "TODO"
+        return f"Rectangle, a={self.a:0.2f}, b={self.b:0.2f}"
 
     @classmethod
     def get_area_formula(cls):
-        return "TODO"
+        return "a * b"
 
     @classmethod
     def get_perimeter_formula(cls):
-        return "TODO"
+        return "2(a + b)"
