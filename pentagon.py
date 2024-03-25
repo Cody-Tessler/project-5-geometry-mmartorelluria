bf70b646bd8bfe723df0474bcf9ccef8e1b4ae9e
mmartorelluria <156358612+mmartorelluria@users.noreply.github.com>
2024-03-25 20:44:18 +0000

    implemnting pentagon.py

--- pentagon.py
+++ pentagon.py
@@ -1,23 +1,27 @@
 from shape import Shape
+import math
 
 class Pentagon(Shape):
 
     def __init__(self, a):
-        ...
+        self.check_if_args_not_below_zero(a)
+        self.a = a
 
     def get_area(self):
-        return 0
+        area = (1 / 4) * math.sqrt(5(5+2*math.sqrt(5))) * self.a ** 2
+        return area
 
     def get_perimeter(self):
-        return 0
+        perimeter = 5 * self.a
+        return perimeter
 
     def __str__(self):
-        return "TODO"
+        return f"Pentagon, a={self.a:0.2f},"
 
     @classmethod
     def get_area_formula(cls):
-        return "TODO"
+        return "(1/4) * sqrt(5(5+2sqrt(5))) * a**2"
 
     @classmethod
     def get_perimeter_formula(cls):
-        return "TODO"
+        return "5a"
