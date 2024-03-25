c96751d646b2c5be21d449b0932cbcc46d4eb22f
mmartorelluria <156358612+mmartorelluria@users.noreply.github.com>
2024-03-25 20:33:40 +0000

    implementing circle.py

--- circle.py
+++ circle.py
@@ -1,23 +1,27 @@
 from shape import Shape
+import math
 
 class Circle(Shape):
 
     def __init__(self, r):
-        ...
-
+        self.check_if_args_not_below_zero(r)
+        self.r = r
+    
     def get_area(self):
-        return 0
+        area = math.pi * self.r ** 2
+        return area
 
     def get_perimeter(self):
-        return 0
+        perimeter = 2 * math.pi * self.r
+        return perimeter
 
     def __str__(self):
-        return "TODO"
+        return f"Circle, r={self.r:0.2f}"
 
     @classmethod
     def get_area_formula(cls):
-        return "TODO"
+        return f"{math.pi} * r ** 2"
 
     @classmethod
     def get_perimeter_formula(cls):
-        return "TODO"
+        return f"2 * {math.pi} * r"
